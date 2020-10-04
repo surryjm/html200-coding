d8e6c31a3ab28787de9571cbb18f4f615d05e750
Surry <50641921+surryjm@users.noreply.github.com>
2020-10-04 14:45:28 -0700

    Finished calculator.js

---
+++ Lesson02/calculator.js
@@ -0,0 +1,20 @@
+let a = 10;
+let b = 20;
+let c = '30';
+let d = '100';
+let e = 'hello';
+let f = 'there';
+
+console.log('1. ' + (a + b));
+console.log('2. ' + (a * b));
+console.log('3. ' + (a / b));
+console.log('4. ' + (a + c));
+console.log('5. ' + (b + d));
+console.log('6. ' + (c * d));
+console.log('7. ' + (c / a));
+console.log('8. ' + (a - b));
+console.log('9. ' + (a / d));
+console.log('10. ' + (d / c)); 
+console.log('11. ' + (e + f));
+console.log('12. ' + (f / a));
+console.log('13. ' + (f * b));
