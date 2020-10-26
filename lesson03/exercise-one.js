33bbb525c00f736730155b92cec8d9b584960546
Surry <50641921+surryjm@users.noreply.github.com>
2020-10-26 08:02:27 -0700

    created exercise-one.js

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,15 @@
+let arr = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+console.log(arr);
+
+arr.push('seven');
+
+console.log(arr);
+
+arr.splice(2, 1);
+
+console.log(arr);
+
+let str = arr.join(',');
+
+console.log(str);
